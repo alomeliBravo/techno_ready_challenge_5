567b39d6fdd20ec58e5832162998bae3d1c9c3f1
Angel Lomeli <alomelibravo@gmail.com>
2025-10-17 22:53:06 -0600

    feat: add ItemCreateDTO record

---
+++ src/main/java/com/pikolic/meli/dto/item/ItemCreateDTO.java
@@ -0,0 +1,12 @@
+package com.pikolic.meli.dto.item;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record ItemCreateDTO(
+        @NotBlank String name,
+        String description,
+        @NotNull @Positive double price
+) {
+}
