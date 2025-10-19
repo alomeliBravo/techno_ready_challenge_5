40817657f4b2ddd995511962520d5a0c306537b9
Angel Lomeli <alomelibravo@gmail.com>
2025-10-18 18:13:52 -0600

    feature: add Order entity

---
+++ src/main/java/com/pikolic/meli/entity/OrderEntity.java
@@ -0,0 +1,31 @@
+package com.pikolic.meli.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private ClientEntity client;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private ItemEntity item;
+    @Column(nullable = false)
+    private LocalDate purchaseDate;
+    @Column(nullable = false)
+    private Double total;
+}
