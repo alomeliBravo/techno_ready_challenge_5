98a33b940b18aed07fb1d00ea758080b6ab48c85
Angel Lomeli <alomelibravo@gmail.com>
2025-10-19 01:05:52 -0600

    fix: add missing public modifier to ItemRepository

--- src/main/java/com/pikolic/meli/repository/ItemRepository.java
+++ src/main/java/com/pikolic/meli/repository/ItemRepository.java
@@ -3,5 +3,5 @@ package com.pikolic.meli.repository;
 import com.pikolic.meli.entity.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
 }
