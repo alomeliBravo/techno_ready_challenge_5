222538d6443b1c1cb8f4112a43fb0b6b1cc0116c
Angel Lomeli <alomelibravo@gmail.com>
2025-10-20 15:00:15 -0600

    fix: add the correct route to OrderController

--- src/main/java/com/pikolic/meli/controller/OrderController.java
+++ src/main/java/com/pikolic/meli/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 class OrderController {
 
