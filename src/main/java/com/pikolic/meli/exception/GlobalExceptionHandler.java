b28ca2d4616fb09e69a71ab5ed909510a58f58e3
Angel Lomeli <alomelibravo@gmail.com>
2025-10-21 02:38:32 -0600

    feature: add valitadion to Invalid Method Arguments Exceptions

--- src/main/java/com/pikolic/meli/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pikolic/meli/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.pikolic.meli.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 class GlobalExceptionHandler {
@@ -19,5 +24,23 @@ class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return ResponseEntity.status(ex.getHttpStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentException(MethodArgumentNotValidException ex, WebRequest request) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                String.join("| ", errors),
+                request.getDescription(false).replace("uri=",""),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
